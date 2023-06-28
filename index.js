8aa07af669e033ea7ad374ce8e105941f703b072
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-06-28 18:47:10 -0400

    bugs are present due to incomplete code

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
 // NOTE TO SELF: put a const svg something here and require the svg stuff you need
 
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Entre up to three characters'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Entre up to three characters'
+    }
+];
+
+
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName,data, (err)=>{
+    fs.writeFile(fileName, data, (err) => {
         err ? console.error(err) : console.log(`Successfully Created ${fileName}`);
     });
 }
 
-function init(){
+function init() {
     inquirer
-    .createPromptModule(questions)
-    .then((answeres)=>{
-        // const data = /* NOTE TO SELF: call the const that you used for SVG stuff and pass in the user input parametes*/
-        writeToFile()
-    })
+        .createPromptModule(questions)
+        .then((answeres) => {
+            // const data = /* NOTE TO SELF: call the const that you used for SVG stuff and pass in the user input parametes*/
+            writeToFile()
+        })
 }
 
 init();
