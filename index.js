548ab9133384018359e94b4bf1f78d7a59f28fad
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-03 14:39:18 -0400

    all functions work, except for writing to file. need to fix it next

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const questions = [{
 },
 {
     type: 'list',
-    name: 'shape',
+    name: 'shapeForm',
     message: 'Chose a shape:',
     choices: ['circle', 'triangle', 'square'],
 },
 {
     type: 'input',
-    name: 'shapeColor',
+    name: 'shapeCol',
     message: 'Enter a shape color keyword (OR a hexadecimal number)',
 }];
 
@@ -40,8 +40,9 @@ function init() {
         .then((answeres) => {
             // const data = JSON.stringify(answeres);
             const data = sVg(answeres);
+            // console.log(data);
             /* NOTE TO SELF: call the const that you used for SVG stuff and pass in the user input parametes*/
-            // writeToFile('logo.svg', data);
+            writeToFile('logo.svg', data);
         });
 }
 
